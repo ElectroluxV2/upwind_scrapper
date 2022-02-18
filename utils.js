6c7672ecfa3887ce00916aebba88d0ac8429c7b6
electroluxv2 <electroluxv2@gmail.com>
2022-02-18 05:23:51 +0100

    SQL extractor

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
+import * as stringSimilarity from 'https://deno.land/x/string_similarity/mod.ts'
 
 export const OPEN_SKIFF_NAME_PATTERN = new RegExp(/o('*)pen\s*(skiff|bic)/i);
 export const UPWIND_API_BASE = 'https://www.upwind24.pl';
@@ -6,3 +7,6 @@ export const get = async endpoint => await (await fetch(`${endpoint.startsWith('
 export const html_to_element = html => new DOMParser().parseFromString(html, 'text/html');
 export const extract_text_from_element = element => element.textContent.trim();
 export const same_length_array_zip = (a, b) => a.map((k, i) => [k, b[i]]);
+export const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
+export const string_similarity = (first, second) => stringSimilarity.compareTwoStrings(first, second);
+export const to_pg_date = timestamp => { const d = new Date(timestamp); return `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')}` };
